303073cc3ae7844d828205de2cb5a697a188d817
Matthew Joseph Fang Bilaos <bilaos02311@gmail.com>
2023-06-12 00:53:59 +0800

    Remove Collapse, Add Default Img

--- driver/camera-platenumber.php
+++ driver/camera-platenumber.php
@@ -51,12 +51,12 @@ if ($riderStatement->rowCount() === 1) {
 ?>
 
 <!-- ======= Why Us Section ======= -->
-<section id="why-us" class="why-us section-bg">
+<section id="rider" class="rider section-bg">
     <div class="container-fluid" data-aos="fade-up">
 
         <div class="row">
 
-            <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+            <div class="col-lg-6 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
 
                 <div class="content">
                     <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
@@ -65,8 +65,13 @@ if ($riderStatement->rowCount() === 1) {
                 <div class="accordion-list">
                     <ul>
                         <li>
-                            <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1"><span>01</span> Driver's Information <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-                            <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
+                            <a class="collapse"><span><?php echo $rider_no ?></span> Driver's Information <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-down icon-close"></i></a>
+                            <div class="collapse show d-flex flex-column">
+                                <?php if ($img_url && file_exists("../assets/img/rider/$img_url")) : ?>
+                                    <div class="pic align-self-center"><img src="../assets/img/rider/<?php echo $img_url ?>" class="img-fluid" alt=""></div>
+                                <?php else : ?>
+                                    <div class="pic"><img src="../assets/img/default-img.svg ?>" class="img-fluid" alt=""></div>
+                                <?php endif; ?>
 
 
                                 <p> Name: <?php echo $name ?></p>
