63bda4cf3e6437ca379442340247c2fc88224356
Matthew Joseph Fang Bilaos <bilaos02311@gmail.com>
2023-06-19 22:00:44 +0800

    Add About Us

--- index.php
+++ index.php
@@ -208,22 +208,31 @@
         <div class="row content">
           <div class="col-lg-6">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+              Welcome to the Plate Number Scanner System for Accredited Mindanao State University Riders! Our system is designed to streamline the process of identifying and verifying authorized riders within the MSU community using Artificial Intelligence technology.
+            </p>
+            <p>
+              At its core, our system utilizes the power of Google Teachable Machine, an innovative machine learning platform. We have trained the system with carefully curated data specific to MSU riders, enabling it to accurately scan and recognize plate numbers associated with authorized vehicles.
+
+
+            </p>
+
+            <p>
+              Key Features
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+
+              <li><i class="ri-check-double-line"></i> Plate Number Recognition</li>
+              <li><i class="ri-check-double-line"></i> User-Friendly Interface</li>
             </ul>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              Developed by a dedicated team of 3rd-year Bachelor of Science in Information Technology students from Mindanao State University - General Santos City(MSU-GenSan), our goal is to enhance campus and student security.
             </p>
-            <a href="#" class="btn-learn-more">Learn More</a>
+            <p>
+              We value feedback and continuously strive to improve our system. If you have any questions, suggestions, or concerns, please don't hesitate to reach out to us. We are here to assist you and ensure a safe and efficient riding experience within Mindanao State University.
+            </p>
+            <a href="mailto:matthewjoseph.bilaos@msugensan.edu.ph" class="btn-learn-more">Email Here</a>
           </div>
         </div>
 
